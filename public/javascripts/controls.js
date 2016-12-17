848407590c826e7e7904f33c6c6e7f4bf529b37d
Kaboyi <kaboyi@163.com>
2016-12-17 14:45:51 +0800

    1

--- public/javascripts/controls.js
+++ public/javascripts/controls.js
@@ -1,3 +1,18 @@
 /**
  * Created by Kaboyi on 2016/12/17.
  */
+"use strict";
+/*
+Controllers
+ */
+angular.module('angularRestfulAuth')
+	.controller('HomeCtrl',
+		['$rooScope', '$scope', '$location', '$localStorage', 'Main',
+		function($rootScope, $scope, $location, $localStorage, Main){
+			$scope.signin=function(){
+				var formData = {
+					email:$scope.email,
+					password:$scope.password
+				}
+			}
+		}])
