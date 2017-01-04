1452e46b0177e125afb0969071fd46c6e937c653
Kaboyi <kaboyi@163.com>
2017-01-04 23:58:03 +0800

    1

--- routes/DRApp.js
+++ routes/DRApp.js
@@ -1,14 +1,14 @@
 /**
  * Created by Kaboyi on 2016/12/22.
  */
-angular.module('DRCtrl',[
+angular.module('DRApp',[
        'ngStorage',
        'ngRoute',
        'angular-loading-bar'])
 .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
     $routeProvider.
         when('/',{
-               templateUrl:'views/demo.jade',
-                controller:'HomeCtrl'
+            templateUrl:'views/demo.jade',
+            controller:'DRHomeCtrl'
     })
 }])
